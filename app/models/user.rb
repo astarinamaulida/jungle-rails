199d90eb1f12d17240f508a2ed996f51eee5cbce
Astarina Maulida <astarina.maulida@gmail.com>
2021-11-17 05:54:55 -0500

    Add user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  def authenticate_with_credentials(email, password)
+    user = User.find_by_email(email)
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
 end
